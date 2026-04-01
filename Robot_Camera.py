ee8a373ab56907626cccdd981c866c9594efb514
Liam Rogers <LiamRogersUK@gmail.com>
2026-04-01 15:54:48 +0100

    add comments & remove redundant lines of code

--- Robot_Camera.py
+++ Robot_Camera.py
@@ -2,16 +2,16 @@
 import cv2
 from picamera2 import Picamera2
 
-#Initilise Camera
+#Initialise Camera
 robotCam = Picamera2()
 robotCam.preview_configuration.main.size = (720, 480) #set image resolution
 robotCam.preview_configuration.main.format = "RGB888" #set colour format ?BGR?
-robotCam.configure("preview")
+robotCam.preview_configuration.controls.FrameRate = 30 #set desired framerate
+robotCam.start() #start camera
 
-robotCam.start()
 while True:
-    frame = robotCam.capture_array()
-    cv2.imshow("Camera Feed", frame)
-    if cv2.waitKey(1) == ord('q'):
+    frame = robotCam.capture_array() #get frame
+    cv2.imshow("Camera Feed", frame) #show frame
+    if cv2.waitKey(1) == ord('q'): #if detect 'q' press
         break
 cv2.destroyAllWindows()
