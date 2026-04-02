d10b1f46de772b05fb67c7f2d86e072914e12ac1
Liam Rogers <LiamRogersUK@gmail.com>
2026-04-02 15:31:15 +0100

    add code to measure fps

--- Robot_Camera.py
+++ Robot_Camera.py
@@ -1,6 +1,7 @@
 
 import cv2
 from picamera2 import Picamera2
+import time
 
 #Initialise Camera
 robotCam = Picamera2()
@@ -10,8 +11,12 @@ robotCam.preview_configuration.controls.FrameRate = 30 #set desired framerate
 robotCam.start() #start camera
 
 while True:
+    tStart = time.time() #time at start
     frame = robotCam.capture_array() #get frame
     cv2.imshow("Camera Feed", frame) #show frame
     if cv2.waitKey(1) == ord('q'): #if detect 'q' press
         break
+    tEnd = time.time() #time at end
+    fps = 1/(tEnd - tStart)
+    print(int(fps)) #make it 'int' to round the number
 cv2.destroyAllWindows()
