40b9a47c4e516f38b7a96619306c88b3520f9e22
Liam Rogers <LiamRogersUK@gmail.com>
2026-04-02 17:20:15 +0100

    define parameters as variables & update comments

--- Robot_Camera.py
+++ Robot_Camera.py
@@ -4,23 +4,32 @@ from picamera2 import Picamera2
 import time
 
 fps = 0
+dispW = 640
+dispH = 360
+pos = (10, 30)
+font = cv2.FONT_HERSHEY_SIMPLEX
+height = 1
+fpsColor = (0, 255, 0) #GREEN, default colour for fps counters
+weight = 3
+
 
 #Initialise Camera
 robotCam = Picamera2()
-robotCam.preview_configuration.main.size = (720, 480) #set image resolution
-robotCam.preview_configuration.main.format = "RGB888" #set colour format ?BGR?
-robotCam.preview_configuration.controls.FrameRate = 30 #set desired framerate
-robotCam.start() #start camera
+robotCam.preview_configuration.main.size = (dispW, dispH)
+robotCam.preview_configuration.main.format = "RGB888" #so I don't need to convert the format later for display purposes
+robotCam.preview_configuration.controls.FrameRate = 30 #desired framerate, may not be actual framerate
+robotCam.start() 
 
+#Display camera feed, with fps
 while True:
     tStart = time.time()
-    frame = robotCam.capture_array()
-    cv2.putText(frame, str(int(fps)), (30, 60), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 3)
-    cv2.imshow("Camera Feed", frame)
+    img = robotCam.capture_array()
+    cv2.putText(img, str(int(fps)), pos, font, height, fpsColor, weight)
+    cv2.imshow("Camera Feed", img)
     if cv2.waitKey(1) == ord('q'): #if detect 'q' press
         break
     tEnd = time.time()
-    loopTime = tEnd - tStart #time for iteration of loop (time at end - time at start)
+    loopTime = tEnd - tStart 
     fps = .9*fps + .1*(1/loopTime) #using low pass filter for cleaner data value
     #print(int(fps)) #make it 'int' to round the number
     
