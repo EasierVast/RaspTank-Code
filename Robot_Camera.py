b5f829462dfd19e931283a62731d0f81aab8d6dd
Liam Rogers <LiamRogersUK@gmail.com>
2026-04-03 16:29:05 +0100

    create function for showing fps counter on camera feed

--- Robot_Camera.py
+++ Robot_Camera.py
@@ -20,17 +20,22 @@ robotCam.preview_configuration.main.format = "RGB888" #so I don't need to conver
 robotCam.preview_configuration.controls.FrameRate = 30 #desired framerate, may not be actual framerate
 robotCam.start() 
 
-#Display camera feed, with fps
+def showFPS(fps, tStart, tEnd):
+    loopTime = tEnd - tStart 
+    fps = .9*fps + .1*(1/loopTime) #using low pass filter for cleaner data value
+    return fps
+    #print(int(fps)) #make it 'int' to round the number
+
+#Display camera feed
 while True:
     tStart = time.time()
     img = robotCam.capture_array()
-    cv2.putText(img, str(int(fps)), pos, font, height, fpsColor, weight)
+    if fps != 0:
+        cv2.putText(img, str(int(fps)), pos, font, height, fpsColor, weight)
     cv2.imshow("Camera Feed", img)
     if cv2.waitKey(1) == ord('q'): #if detect 'q' press
         break
     tEnd = time.time()
-    loopTime = tEnd - tStart 
-    fps = .9*fps + .1*(1/loopTime) #using low pass filter for cleaner data value
-    #print(int(fps)) #make it 'int' to round the number
+    fps = showFPS(fps, tStart, tEnd)
     
 cv2.destroyAllWindows()
