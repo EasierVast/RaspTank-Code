8cdd46fc80eaabf554bd507a3313259e3f9b47ce
Liam Rogers <LiamRogersUK@gmail.com>
2026-04-08 15:01:54 +0100

    modify calculations in 'calcFPS' and reimplement 'showFPS' function

--- Robot_Camera.py
+++ Robot_Camera.py
@@ -4,14 +4,9 @@ from picamera2 import Picamera2
 import time
 
 fps = 0
+prevTime = 0
 dispW = 640
 dispH = 360
-pos = (10, 30)
-font = cv2.FONT_HERSHEY_SIMPLEX
-height = 1
-fpsColor = (0, 255, 0) #GREEN, default colour for fps counters
-weight = 3
-
 
 #Initialise Camera
 robotCam = Picamera2()
@@ -20,22 +15,31 @@ robotCam.preview_configuration.main.format = "RGB888" #so I don't need to conver
 robotCam.preview_configuration.controls.FrameRate = 30 #desired framerate, may not be actual framerate
 robotCam.start() 
 
-def calcFPS(fps, tStart):
-    tEnd = time.time()
-    loopTime = tEnd - tStart 
-    fps = .9*fps + .1*(1/loopTime) #using low pass filter for cleaner data value
-    return fps
+def calcFPS(fps, prevTime):
+    currTime = time.time()
+    timeDiff = currTime - prevTime 
+    fps = .9*fps + .1*(1/timeDiff) #using low pass filter for cleaner data value
+    prevTime = currTime
+    return fps, prevTime
+
+def showFPS(fps, prevTime, img):
+    pos = (10, 30)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    height = 1
+    color = (0, 255, 0) #GREEN, default colour for fps counters
+    weight = 3
+    
+    fps, prevTime = calcFPS(fps, prevTime)
+    cv2.putText(img, str(int(fps)), pos, font, height, color, weight)
+    return fps, prevTime, img
     #print(int(fps)) #make it 'int' to round the number
 
 #Display camera feed
 while True:
-    tStart = time.time()
     img = robotCam.capture_array()
-    if fps != 0:
-        cv2.putText(img, str(int(fps)), pos, font, height, fpsColor, weight)
+    fps, prevTime, img = showFPS(fps, prevTime, img)
     cv2.imshow("Camera Feed", img)
     if cv2.waitKey(1) == ord('q'): #if detect 'q' press
         break
-    fps = calcFPS(fps, tStart)
     
 cv2.destroyAllWindows()
