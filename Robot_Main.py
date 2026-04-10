d9987a7e38c6aceeb04b6c07ad30395956ea626c
Liam Rogers <LiamRogersUK@gmail.com>
2026-04-10 16:00:31 +0100

    camera feed now viewable from main script

--- Robot_Main.py
+++ Robot_Main.py
@@ -3,8 +3,10 @@ from board import SCL, SDA
 import busio
 from adafruit_pca9685 import PCA9685
 from adafruit_motor import motor
+import cv2
 from time import sleep
 import Robot_Move as move
+import Robot_Camera as cam
 
 speed = 0.5 #1 = full speed
 
@@ -22,9 +24,14 @@ def robotMove(direction):
         print("Invalid Input")
 
 try:
-    while True:
-        direction = input("Input direction:")
-        robotMove(direction)
+	robotCam = cam.initCam()
+	while True:
+		img = robotCam.capture_array()
+		cv2.imshow("Camera Feed", img)
+		if cv2.waitKey(1) == ord('q'): #if detect 'q' press
+			break
+		#direction = input("Input direction:")
+		#robotMove(direction)
 except KeyboardInterrupt: #ctrl+C to stop code
     print("EXIT LOOP")
     move.stopTurn() #make sure motor is off
