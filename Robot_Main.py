990d8d7b076a7d0206ec20256acc91e78870e316
Liam Rogers <LiamRogersUK@gmail.com>
2026-04-22 14:40:52 +0100

    Call 'showFPS' function to main script The variables for 'fps' and 'prevtime' needed to be defined with the main script for the function to work. The line of code to show the camera feed was also moved to the end of script so that any changes to the feed can applied before it is displayed.

--- Robot_Main.py
+++ Robot_Main.py
@@ -9,6 +9,8 @@ import Robot_Move as move
 import Robot_Camera as cam
 
 speed = 0.5 #1 = full speed
+fps = 0
+prevTime = 0
 
 #Move the robot    
 def robotMove(direction):
@@ -27,11 +29,12 @@ try:
 	robotCam = cam.initCam()
 	while True:
 		img = robotCam.capture_array()
-		cv2.imshow("Camera Feed", img)
 		if cv2.waitKey(1) == ord('q'): #if detect 'q' press
 			break
 		#direction = input("Input direction:")
 		#robotMove(direction)
+		fps, prevTime, img = cam.showFPS(fps, prevTime, img)
+		cv2.imshow("Camera Feed", img)
 except KeyboardInterrupt: #ctrl+C to stop code
     print("EXIT LOOP")
     move.stopTurn() #make sure motor is off
