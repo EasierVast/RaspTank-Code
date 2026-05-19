512b41666cebd4042520de4519d00684ebc104c7
Liam Rogers <LiamRogersUK@gmail.com>
2026-05-19 16:03:31 +0100

    Create basic code to control a servo motor This module will contain all servo motor control scripts for easy import into the main script. Currently, it sets the shoulder servo to a 90 degree angle.

---
+++ Robot_Servo.py
@@ -0,0 +1,21 @@
+
+from board import SCL, SDA
+import busio
+from adafruit_pca9685 import PCA9685
+from adafruit_motor import servo
+from time import sleep
+
+shoulderPin = 0
+
+PWM = 50 #PWM frequency to 50hz (standard value for servo)
+
+#Initialize I2C bus using busio
+i2c = busio.I2C(SCL, SDA)
+
+#Create a simple PCA9685 class instance for the Servo Driver
+pca = PCA9685(i2c, address=0x5f) #default 0x40 but HAT uses 0x5f
+pca.frequency = PWM
+
+#Intialize servo & set angle
+servoShoulder = servo.Servo(pca.channels[shoulderPin], min_pulse=500, max_pulse=2500, actuation_range=180)
+servoShoulder.angle = 90
