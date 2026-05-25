d2a628294fc1288f40d939c496b9b51baf0e15f1
Liam Rogers <LiamRogersUK@gmail.com>
2026-05-25 15:59:14 +0100

    Further calibrate claw angle The servo in the claw was still making a stuttering sound when at an angle of 100, this may indicate that the fingers of the claw are being pressed against each other too tightly. For the sake of safety, to avoid damaging the servo, the angle has been set to 103.

--- Robot_Servo.py
+++ Robot_Servo.py
@@ -8,7 +8,7 @@ from time import sleep
 shoulderPin = 0
 elbowPin = 1
 wristPin = 2
-clawPin = 3 #closed claw at 100
+clawPin = 3 #closed claw at 100 (103 for safety)
 camPin = 4 #movement range 50 - 100
 
 PWM = 50 #PWM frequency to 50hz (standard value for servo)
@@ -30,7 +30,7 @@ if __name__ == '__main__': #Test Code
 	setServoAngle(shoulderPin, 20)
 	setServoAngle(elbowPin, 40)
 	setServoAngle(wristPin, 90)
-	setServoAngle(clawPin, 100)
+	setServoAngle(clawPin, 103)
 	setServoAngle(camPin, 90)
 	
 	
