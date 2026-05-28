b2574172615f6a0764c3a4975a6f2adc4196bc4c
Liam Rogers <LiamRogersUK@gmail.com>
2026-05-28 16:40:46 +0100

    Implement Servo class The servo configuration is now managed through a dedicated Servo class, treating each servo as an independent object. This approach encapsulates the assigned pin and initial angle within each servo instance, keeping the information organised.

--- Robot_Servo.py
+++ Robot_Servo.py
@@ -5,6 +5,7 @@ from adafruit_pca9685 import PCA9685
 from adafruit_motor import servo
 from time import sleep
 
+"""
 #Servo Pins
 shoulderPin = 0
 elbowPin = 1
@@ -18,6 +19,7 @@ elbowAngle = 40
 wristAngle = 90
 clawAngle = 103
 camAngle = 90
+"""
 
 PWM = 50 #PWM frequency to 50hz (standard value for servo)
 
@@ -28,17 +30,29 @@ i2c = busio.I2C(SCL, SDA)
 pca = PCA9685(i2c, address=0x5f) #default 0x40 but HAT uses 0x5f
 pca.frequency = PWM
 
-def setServoAngle(ID, angle):
-	#Intialize servo & set angle
-	servoAngle = servo.Servo(pca.channels[ID], min_pulse=200, max_pulse=2400, actuation_range=180)
-	servoAngle.angle = angle
+class Servo:
+	def __init__(self, pin, angle):
+		self.pin = pin
+		self.angle = angle
+		
+	def setServoAngle(self):
+		#Intialize servo & set angle
+		servoAngle = servo.Servo(pca.channels[self.pin], min_pulse=200, max_pulse=2400, actuation_range=180)
+		servoAngle.angle = self.angle
+
+#Servo Objects		
+shoulder = Servo(pin = 0, angle = 20)
+elbow = Servo(pin = 1, angle = 40)
+wrist = Servo(pin = 2, angle = 90)
+claw = Servo(pin = 3, angle = 103)
+cam = Servo(pin = 4, angle = 90)
 	
 def initPosition():
-	setServoAngle(shoulderPin, shoulderAngle)
-	setServoAngle(elbowPin, elbowAngle)
-	setServoAngle(wristPin, wristAngle)
-	setServoAngle(clawPin, clawAngle)
-	setServoAngle(camPin, camAngle)
+	shoulder.setServoAngle()
+	elbow.setServoAngle()
+	wrist.setServoAngle()
+	claw.setServoAngle()
+	cam.setServoAngle()
 
 if __name__ == '__main__': #Test Code
 	
